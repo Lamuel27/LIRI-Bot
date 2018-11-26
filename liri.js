67370e35a639eb1c5ddeb49b8f49bd8ef29c338e
Lamuel27 <samrelliner@gmail.com>
2018-11-26 11:26:13 -0600

    bands

--- liri.js
+++ liri.js
@@ -1,1 +1,77 @@
 require("dotenv").config();
+
+//get the keys.js file
+var keys = require("./keys.js");
+
+//require node packages
+var spotify1 = require("node-spotify-api");
+var request = require("request");
+var moment = require("moment");
+var fs = require("fs");
+
+//attach the key to a variable
+var spotify2 = new spotify1(keys.spotify);
+
+//command line arguments
+var arg = process.argv;
+
+//user input commands
+var userInput1 = "";
+var userInput2 = "";
+
+// get the user input for everything via forloop and make it so you can type more than one word
+for (var i = 3; i < arg.length; i++) {
+    if (i > 3 && i < arg.length) {
+        userInput1 = userInput1 + "%20" + arg[i];
+    }
+    else {
+        userInput1 += arg[i];
+    }
+}
+
+var command1 = process.argv[2];
+console.log(command1);
+console.log(process.argv);
+liriBot();
+
+// switch statements
+function liriBot() {
+    switch (command1) {
+        case "concert-this":
+
+            //call to API 
+            var queryURL = "https://rest.bandsintown.com/artists/" + userInput1 + "/events?app_id=codingbootcamp"
+            request(queryURL, function (error, response, body) {
+                //If no error and response is a success
+                if (!error && response.statusCode === 200) {
+                    //Parse the json response
+                    var data = JSON.parse(body);
+                    //create a forloop for array
+                    for (var i = 0; i < data.length; i++) {
+
+                        //venue name
+                        console.log("Venue: " + data[i].venue.name);
+
+                        // venue location
+                        if (data[i].venue.region == "") {
+                            console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+
+                        } else {
+                            console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
+
+                        }
+
+                        //date of show
+                        var showDate = data[i].datetime;
+                        showDate = moment(showDate).format("MM/DD/YYYY");
+                        console.log("Date: " + showDate)
+
+                        console.log("////////////////////")
+                    }
+                }
+            });
+
+            break;
+    }
+}
+
