1ffbb43c5a411eb54ea059280ec01951da047258
Lamuel27 <samrelliner@gmail.com>
2018-11-27 22:54:50 -0600

    movies

--- liri.js
+++ liri.js
@@ -22,7 +22,7 @@ var userInput2 = "";
 // get the user input for everything via forloop and make it so you can type more than one word
 for (var i = 3; i < arg.length; i++) {
     if (i > 3 && i < arg.length) {
-        userInput1 = userInput1 + "%20" + arg[i];
+        userInput1 = userInput1 + "+" + arg[i];
     }
     else {
         userInput1 += arg[i];
@@ -72,6 +72,31 @@ function liriBot() {
             });
 
             break;
+        case "movie-this":
+            //If statement for no movie provided
+            if (!userInput1) {
+                userInput1 = "Mr%20Nobody";
+                userInput2 = userInput1.replace(/%20/g, " ");
+            }
+
+            var queryURL = "https://www.omdbapi.com/?t=" + userInput1 + "&y=&plot=short&apikey=584a2704"
+            request(queryURL, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var data = JSON.parse(body);
+                    console.log("Title: " + data.Title)
+                    console.log("Release Year: " + data.Year)
+                    console.log("OMDB Rating: " + data.Ratings[0].Value)
+                    console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value)
+                    console.log("Country of Production: " + data.Country)
+                    console.log("Language: " + data.Language)
+                    console.log("Plot of the Movie: " + data.Plot)
+                    console.log("Actors: " + data.Actors)
+
+                }
+            });
+
+            break;
+
     }
 }
 
