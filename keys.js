9122f45925f5dcf8c55e701ae1d9e5fe6b41f4ea
Lamuel27 <samrelliner@gmail.com>
2018-12-01 12:22:24 -0600

    liri illistration

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+console.log(' _        _____   _____    _____ \n| |      |_   _| |  __ \\  |_   _|\n| |        | |   | |__) |   | |  \n| |        | |   |  _  /    | |  \n| |____   _| |_  | | \\ \\   _| |_ \n|______| |_____| |_|  \\_\\ |_____|');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
